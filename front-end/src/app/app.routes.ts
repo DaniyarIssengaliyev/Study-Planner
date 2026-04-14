d57f5e14b08be34a7fa5722e6c39032594e19ff0
DaniyarIssengaliyev <youiseni@gmail.com>
2026-04-14 16:43:23 +0500

    commit

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { DashboardPage } from './pages/dashboards/dashboard-page';
+import { TasksPage } from './pages/tasks/tasks-page';
+import { SubjectsPage } from './pages/subjects/subjects-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardPage,
+  },
+  {
+    path: 'tasks',
+    component: TasksPage,
+  },
+  {
+    path: 'subjects',
+    component: SubjectsPage,
+  },
+];
---
+++ front-end/src/app/pages/subjects/subjects-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subjects-page',
+  standalone: true,
+  templateUrl: './subjects-page.html',
+  styleUrl: './subjects-page.css',
+})
+export class SubjectsPage {}
