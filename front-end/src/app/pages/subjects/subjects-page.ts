53d6abe7bda13b658edb4e85acfb36890c1eb0e0
DaniyarIssengaliyev <youiseni@gmail.com>
2026-04-14 23:43:20 +0500

    commit

--- front-end/src/app/pages/subjects/subjects-page.ts
+++ front-end/src/app/pages/subjects/subjects-page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { Subject } from '../../models/api.models';
 
 @Component({
   selector: 'app-subjects-page',
@@ -6,4 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './subjects-page.html',
   styleUrl: './subjects-page.css',
 })
-export class SubjectsPage {}
+export class SubjectsPage implements OnInit {
+  private api = inject(ApiService);
+
+  subjects: Subject[] = [];
+  errorMessage = '';
+
+  ngOnInit(): void {
+    this.loadSubjects();
+  }
+
+  loadSubjects(): void {
+    this.api.getSubjects().subscribe({
+      next: (data) => {
+        this.subjects = data;
+        this.errorMessage = '';
+      },
+      error: (err) => {
+        console.error('Error loading subjects:', err);
+        this.errorMessage = 'Failed to load subjects';
+      },
+    });
+  }
+}
